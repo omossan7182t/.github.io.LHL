308aab4933da1fa32480236f32c2f99ac54f4357
omossan7182t <omossan7182t@gmail.com>
2026-01-12 09:58:14 +0900

    Update LanguageRegistry.js

--- LanguageRegistry.js
+++ LanguageRegistry.js
@@ -1,19 +1,25 @@
 export class LanguageRegistry {
-  private static languages = new Map<string, LanguageDef>();
+  static languages = new Map();
 
-  static register(lang: LanguageDef) {
+  static register(lang) {
     this.validate(lang);
     this.languages.set(lang.id, lang);
   }
 
-  static get(id: string) {
+  static get(id) {
     const lang = this.languages.get(id);
-    if (!lang) throw new Error(`Language not found: ${id}`);
+    if (!lang) {
+      throw new Error(`Language not found: ${id}`);
+    }
     return lang;
   }
 
-  static validate(lang: LanguageDef) {
-    if (!lang.id) throw new Error("language.id is required");
-    if (!lang.commands) throw new Error("language.commands is required");
+  static validate(lang) {
+    if (!lang.id) {
+      throw new Error("language.id is required");
+    }
+    if (!lang.commands) {
+      throw new Error("language.commands is required");
+    }
   }
 }
