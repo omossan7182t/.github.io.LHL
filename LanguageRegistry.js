40af7dcedbb5b4566167a376db26ae0163851862
omossan7182t <omossan7182t@gmail.com>
2026-01-12 17:05:23 +0900

    Update LanguageRegistry.js

--- LanguageRegistry.js
+++ LanguageRegistry.js
@@ -8,18 +8,17 @@ export class LanguageRegistry {
 
   static get(id) {
     const lang = this.languages.get(id);
-    if (!lang) {
-      throw new Error(`Language not found: ${id}`);
-    }
+    if (!lang) throw new Error(`Language not found: ${id}`);
     return lang;
   }
 
+  static getAll() {
+    return Array.from(this.languages.values());
+  }
+
   static validate(lang) {
-    if (!lang.id) {
-      throw new Error("language.id is required");
-    }
-    if (!lang.commands) {
-      throw new Error("language.commands is required");
-    }
+    if (!lang.id) throw new Error("language.id is required");
+    if (!lang.name) throw new Error("language.name is required");
+    if (!lang.commands) throw new Error("language.commands is required");
   }
 }
