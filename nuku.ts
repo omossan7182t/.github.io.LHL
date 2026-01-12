914b8a104fd634a6edfc8667d3b432955f89b7f0
omossan7182t <omossan7182t@gmail.com>
2026-01-12 09:47:50 +0900

    Create nuku.ts

---
+++ nuku.ts
@@ -0,0 +1,14 @@
+export const NukuLanguage = {
+  id: "nuku",
+  name: "ぬく方言",
+  commands: {
+    "+": { op: "ADD", delta: 1 },
+    "-": { op: "ADD", delta: -1 },
+    ">": { op: "MOVE", delta: 1 },
+    "<": { op: "MOVE", delta: -1 },
+    ".": { op: "OUTPUT" },
+    ",": { op: "INPUT" },
+    "[": { op: "LOOP_START" },
+    "]": { op: "LOOP_END" },
+  },
+};
