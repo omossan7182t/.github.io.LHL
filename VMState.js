85119b95b5945aebea15972f92a216d3cf80f26b
omossan7182t <omossan7182t@gmail.com>
2026-01-13 10:56:34 +0900

    Update VMState.js

--- VMState.js
+++ VMState.js
@@ -10,27 +10,53 @@ export class VMState {
 
   step() {
     const token = this.tokens[this.ip];
-    if (!token) return "END";
+    if (!token) return { state: "END" };
 
     switch (token.op) {
       case "ADD":
         this.memory[this.ptr] += token.delta ?? 0;
+        this.ip++;
         break;
 
       case "MOVE":
         this.ptr += token.delta ?? 0;
+        this.ip++;
         break;
 
       case "OUTPUT":
         console.log(String.fromCharCode(this.memory[this.ptr]));
+        this.ip++;
         break;
 
       case "INPUT":
         // 未実装
+        this.ip++;
         break;
+
+      case "LOOP_START":
+        if (this.memory[this.ptr] === 0) {
+          this.ip = token.jump + 1;
+        } else {
+          this.ip++;
+        }
+        break;
+
+      case "LOOP_END":
+        if (this.memory[this.ptr] !== 0) {
+          this.ip = token.jump + 1;
+        } else {
+          this.ip++;
+        }
+        break;
+
+      default:
+        return {
+          state: "ERROR",
+          message: `Unknown op: ${token.op}`,
+          ip: this.ip
+        };
     }
 
-    this.ip++;
-    return "RUNNING";
+    return { state: "RUNNING" };
   }
 }
