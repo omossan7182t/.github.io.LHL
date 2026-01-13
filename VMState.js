0c697b1f53b6166f78cd9f09e7624d96e436c980
omossan7182t <omossan7182t@gmail.com>
2026-01-13 10:54:03 +0900

    Update VMState.js

--- VMState.js
+++ VMState.js
@@ -3,48 +3,34 @@ export class VMState {
   ptr = 0;
   ip = 0;
   tokens = [];
-  lastOutput = "";
-  ipLine = 0;
 
   constructor(tokens) {
     this.tokens = tokens;
   }
 
-  static fromTokenizeResult(tokens) {
-    return new VMState(tokens);
-  }
-
   step() {
-    this.lastOutput = "";
     const token = this.tokens[this.ip];
     if (!token) return "END";
 
-    try {
-      switch (token.op) {
-        case "ADD":
-          this.memory[this.ptr] += token.delta ?? 0;
-          break;
-        case "MOVE":
-          this.ptr += token.delta ?? 0;
-          break;
-        case "OUTPUT":
-          this.lastOutput = String.fromCharCode(this.memory[this.ptr]);
-          break;
-        case "INPUT":
-          break;
-        case "LOOP_START":
-          if (this.memory[this.ptr] === 0) this.ip = token.jump ?? this.ip;
-          break;
-        case "LOOP_END":
-          if (this.memory[this.ptr] !== 0) this.ip = token.jump ?? this.ip;
-          break;
-      }
-    } catch (e) {
-      return "ERROR";
+    switch (token.op) {
+      case "ADD":
+        this.memory[this.ptr] += token.delta ?? 0;
+        break;
+
+      case "MOVE":
+        this.ptr += token.delta ?? 0;
+        break;
+
+      case "OUTPUT":
+        console.log(String.fromCharCode(this.memory[this.ptr]));
+        break;
+
+      case "INPUT":
+        // 未実装
+        break;
     }
 
     this.ip++;
-    this.ipLine = this.ip; 
     return "RUNNING";
   }
 }
